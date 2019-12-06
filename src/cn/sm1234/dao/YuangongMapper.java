983757d398b2a8ca71dea47f6454f7d0d2d20c0b
Administrator <Administrator@PC-20190713PQBW>
2019-12-06 08:57:31 +0800

    上传

---
+++ src/cn/sm1234/dao/YuangongMapper.java
@@ -0,0 +1,13 @@
+package cn.sm1234.dao;
+
+import java.util.List;
+
+import cn.sm1234.domain.Yuangong;
+
+
+public interface YuangongMapper {
+	/**
+	 * 查询所有数据
+	 */
+	public List<Yuangong> findAll();
+}
---
+++ src/cn/sm1234/service/YuangongService.java
@@ -0,0 +1,12 @@
+package cn.sm1234.service;
+
+import java.util.List;
+
+import cn.sm1234.domain.Yuangong;
+
+public interface YuangongService {
+	/**
+	 * 查询所有数据
+	 */
+	public List<Yuangong> findAll();
+}
