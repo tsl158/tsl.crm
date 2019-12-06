9ecc2feccfd69775ba12678003d2935251f45769
tsl158 <50016210+tsl158@users.noreply.github.com>
2019-12-06 11:39:44 +0800

    Revert "Revert "上传""
    This reverts commit 6e5aa08cba4bcecc811b44871126e59b1bf00aba.

---
+++ src/cn/sm1234/service/CustomerService.java
@@ -0,0 +1,26 @@
+package cn.sm1234.service;
+
+import java.util.List;
+
+import cn.sm1234.domain.Customer;
+
+
+public interface CustomerService {
+
+	/**
+	 * 查询所有数据
+	 */
+	public List<Customer> findAll();
+
+	public void save(Customer customer);
+
+	public Customer findById(Integer id);
+
+	public void delete(Integer[] id);
+	
+	/**
+	 * 导出数据表格显示的所有数据
+	 * @return
+	 */
+	List<Customer> selectPoi();
+}
